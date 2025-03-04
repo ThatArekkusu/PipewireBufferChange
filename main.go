e62dfa10e82d6a16f81d3ed88f0249a58b6f2e8d
ThatArekkusu <arekkusu5665@gmail.com>
2025-03-04 16:08:48 +0000

    Rewrote main.go to be more user friendly

--- main.go
+++ main.go
@@ -9,88 +9,53 @@ import (
 )
 
 func main() {
-	var clockChoice string
 	var clockRate int
 	var dir string
 	var err error
-	var storeDir string
-	var changeDir string
 	var bufferSize int
+	var funcChoice string
+	var funcAppend string
 
 	err = godotenv.Load()
 	if err != nil {
 		fmt.Println("Error loading .env file")
 	}
 
-	dir = os.Getenv("PIPEWIRE_DIR")
-	if dir == "" {
-		fmt.Println("Enter your pipewire configuration directory, This will likely be in /usr/share/pipewire or /.config/pipewire: ")
-		fmt.Scanln(&dir)
+	funcMap := make(map[string]func())
 
-		fmt.Println("Would you like to store your pipewire configuration directory? (y/n): ")
-		fmt.Scanln(&storeDir)
+	funcMap["bufferSet"] = func() { bufferSet(bufferSize, dir, &err) }
+	funcMap["storeDirec"] = func() { storeDirec(dir, &err) }
+	funcMap["clockSet"] = func() { clockSet(clockRate, dir, &err) }
 
-		if storeDir == "y" {
-			storeDirec(dir, &err)
-		}
+	fmt.Println("[1] Set buffer size")
+	fmt.Println("[2] Store directory")
+	fmt.Println("[3] Change directory")
+	fmt.Println("[4] Exit")
+	fmt.Println("Enter your choice: ")
+	fmt.Scanln(&funcChoice)
+
+	switch funcChoice {
+	case "1":
+		funcAppend = "bufferSet"
+	case "2":
+		funcAppend = "storeDirec"
+	case "3":
+		funcAppend = "clockSet"
+	case "4":
+		os.Exit(0)
+	default:
+		fmt.Println("Invalid choice")
 	}
 
-	if dir != "" {
-		fmt.Println("Would you like to change your pipewire configuration directory? (y/n): ")
-		fmt.Scanln(&changeDir)
+	funcMap[funcAppend]()
+}
 
-		if changeDir == "y" {
-			fmt.Println("Enter your pipewire configuration directory, This will likely be in /usr/share/pipewire or /.config/pipewire: ")
-			fmt.Scanln(&dir)
-			storeDirec(dir, &err)
-		}
-	}
-
-	fmt.Println("Do you want to set clock rate? (y/n): ")
-	fmt.Scanln(&clockChoice)
-
-	if clockChoice == "y" {
-		fmt.Println("Enter the clock rate you want to set the system to: ")
-
-		// Define the clock rate the user wants their system to be set to.
-		_, err = fmt.Scanln(&clockRate)
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
-
-		// Uncomment and update the clock rate
-		cmd := exec.Command(
-			"sed",
-			"-i",
-			`/default.clock.rate[[:space:]]*=[[:space:]]*[0-9]*/s/^[#[:space:]]*/    /; s/default.clock.rate[[:space:]]*=[[:space:]]*[0-9]*/default.clock.rate = `+fmt.Sprintf("%d", clockRate)+`/`,
-			dir+"/pipewire.conf")
-
-		out, err := cmd.CombinedOutput()
-		if err != nil {
-			fmt.Println("Error:", err, "If /etc/pipewire it likely will be .conf.d files, try /.config/pipewire")
-		}
-		fmt.Println(string(out))
-
-		// Uncomment and update the allowed rates
-		cmd1 := exec.Command(
-			"sed",
-			"-i",
-			fmt.Sprintf(`/default.clock.allowed-rates[[:space:]]*=[[:space:]]*\[[[:space:]]*[0-9,[:space:]]*\]/s/^[#[:space:]]*/    /; s/default.clock.allowed-rates[[:space:]]*=[[:space:]]*\[[[:space:]]*[0-9,[:space:]]*\]/default.clock.allowed-rates = \[ %d \]/`, clockRate),
-			dir+"/pipewire.conf")
-
-		out1, err := cmd1.CombinedOutput()
-		if err != nil {
-			fmt.Println("Error:", err, "If /etc/pipewire it likely will be .conf.d files, try /.config/pipewire")
-		}
-		fmt.Println(string(out1))
-	}
-
-	fmt.Println("Enter the buffer size you want to set the system to:")
+func bufferSet(bufferSize int, dir string, err *error) {
+	fmt.Println("Enter the buffer size you want to set the system to: ")
 
 	// Define the buffer size the user wants their system to be set to.
-	_, err = fmt.Scanln(&bufferSize)
-	if err != nil {
+	_, errScan := fmt.Scanln(&bufferSize)
+	if errScan != nil {
 		fmt.Println("Error:", err, "If /etc/pipewire it likely will be .conf.d files, try /.config/pipewire")
 		return
 	}
@@ -106,8 +71,8 @@ func main() {
 		`/default.clock.quantum[[:space:]]*=[[:space:]]*[0-9]*/s/^[#[:space:]]*/    /; s/default.clock.quantum[[:space:]]*=[[:space:]]*[0-9]*/default.clock.quantum = `+fmt.Sprintf("%d", bufferSize)+`/`,
 		dir+"/pipewire.conf")
 
-	out1, err := cmd1.CombinedOutput()
-	if err != nil {
+	out1, errCmd := cmd1.CombinedOutput()
+	if errCmd != nil {
 		fmt.Println("Error:", err, "If /etc/pipewire it likely will be .conf.d files, try ~/.config/pipewire")
 	}
 	fmt.Println(string(out1))
@@ -119,8 +84,8 @@ func main() {
 		`/default.clock.min-quantum[[:space:]]*=[[:space:]]*[0-9]*/s/^[#[:space:]]*/    /; s/default.clock.min-quantum[[:space:]]*=[[:space:]]*[0-9]*/default.clock.min-quantum = `+fmt.Sprintf("%d", bufferSizeMinQuantum)+`/`,
 		dir+"/pipewire.conf")
 
-	out2, err := cmd2.CombinedOutput()
-	if err != nil {
+	out2, errCmd := cmd2.CombinedOutput()
+	if errCmd != nil {
 		fmt.Println("Error:", err, "If /etc/pipewire it likely will be .conf.d files, try /.config/pipewire")
 	}
 	fmt.Println(string(out2))
@@ -132,8 +97,8 @@ func main() {
 		`/default.clock.max-quantum[[:space:]]*=[[:space:]]*[0-9]*/s/^[#[:space:]]*/    /; s/default.clock.max-quantum[[:space:]]*=[[:space:]]*[0-9]*/default.clock.max-quantum = `+fmt.Sprintf("%d", bufferSize)+`/`,
 		dir+"/pipewire.conf")
 
-	out3, err := cmd3.CombinedOutput()
-	if err != nil {
+	out3, errCmd := cmd3.CombinedOutput()
+	if errCmd != nil {
 		fmt.Println("Error:", err, "If /etc/pipewire it likely will be .conf.d files, try /.config/pipewire")
 	}
 	fmt.Println(string(out3))
@@ -145,8 +110,8 @@ func main() {
 		`/default.clock.quantum-limit[[:space:]]*=[[:space:]]*[0-9]*/s/^[#[:space:]]*/    /; s/default.clock.quantum-limit[[:space:]]*=[[:space:]]*[0-9]*/default.clock.quantum-limit = `+fmt.Sprintf("%d", bufferSize)+`/`,
 		dir+"/pipewire.conf")
 
-	out4, err := cmd4.CombinedOutput()
-	if err != nil {
+	out4, errCmd := cmd4.CombinedOutput()
+	if errCmd != nil {
 		fmt.Println("Error:", err, "If /etc/pipewire it likely will be .conf.d files, try /.config/pipewire")
 	}
 	fmt.Println(string(out4))
@@ -159,8 +124,8 @@ func main() {
 		dir+"/pipewire-pulse.conf",
 	)
 
-	out5, err := cmd5.CombinedOutput()
-	if err != nil {
+	out5, errCmd := cmd5.CombinedOutput()
+	if errCmd != nil {
 		fmt.Println("Error:", err, "If /etc/pipewire it likely will be .conf.d files, try /.config/pipewire")
 	}
 	fmt.Println(string(out5))
@@ -173,8 +138,8 @@ func main() {
 		dir+"/pipewire-pulse.conf",
 	)
 
-	out6, err := cmd6.CombinedOutput()
-	if err != nil {
+	out6, errCmd := cmd6.CombinedOutput()
+	if errCmd != nil {
 		fmt.Println("Error:", err, "If /etc/pipewire it likely will be .conf.d files, try /.config/pipewire")
 	}
 	fmt.Println(string(out6))
@@ -187,8 +152,8 @@ func main() {
 		dir+"/jack.conf",
 	)
 
-	out7, err := cmd7.CombinedOutput()
-	if err != nil {
+	out7, errCmd := cmd7.CombinedOutput()
+	if errCmd != nil {
 		fmt.Println("Error:", err, "If /etc/pipewire it likely will be .conf.d files, try /.config/pipewire")
 	}
 	fmt.Println(string(out7))
@@ -201,8 +166,8 @@ func main() {
 		dir+"/jack.conf",
 	)
 
-	out8, err := cmd8.CombinedOutput()
-	if err != nil {
+	out8, errCmd := cmd8.CombinedOutput()
+	if errCmd != nil {
 		fmt.Println("Error:", err, "If /etc/pipewire it likely will be .conf.d files, try /.config/pipewire")
 	}
 	fmt.Println(string(out8))
@@ -217,13 +182,16 @@ func main() {
 	envMap["PREV_BUFFER"] = bufferSizeStr
 
 	// Write the updated environment variables back to the .env file
-	err = godotenv.Write(envMap, ".env")
-	if err != nil {
+	errEnv := godotenv.Write(envMap, ".env")
+	if errEnv != nil {
 		fmt.Println("Error writing to .env file")
 	}
 }
 
 func storeDirec(dir string, err *error) {
+	fmt.Println("Enter your pipewire configuration directory, This will likely be in /usr/share/pipewire or /.config/pipewire: ")
+	fmt.Scanln(&dir)
+
 	// Load existing environment variables
 	envMap, loadErr := godotenv.Read()
 	if loadErr != nil {
@@ -238,4 +206,41 @@ func storeDirec(dir string, err *error) {
 	if *err != nil {
 		fmt.Println("Error writing to .env file")
 	}
+}
+
+func clockSet(clockRate int, dir string, err *error) {
+	fmt.Println("Enter the clock rate you want to set the system to: ")
+
+	// Define the clock rate the user wants their system to be set to.
+	_, errScan := fmt.Scanln(&clockRate)
+	if errScan != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Uncomment and update the clock rate
+	cmd := exec.Command(
+		"sed",
+		"-i",
+		`/default.clock.rate[[:space:]]*=[[:space:]]*[0-9]*/s/^[#[:space:]]*/    /; s/default.clock.rate[[:space:]]*=[[:space:]]*[0-9]*/default.clock.rate = `+fmt.Sprintf("%d", clockRate)+`/`,
+		dir+"/pipewire.conf")
+
+	out, errCmd := cmd.CombinedOutput()
+	if errCmd != nil {
+		fmt.Println("Error:", err, "If /etc/pipewire it likely will be .conf.d files, try /.config/pipewire")
+	}
+	fmt.Println(string(out))
+
+	// Uncomment and update the allowed rates
+	cmd1 := exec.Command(
+		"sed",
+		"-i",
+		fmt.Sprintf(`/default.clock.allowed-rates[[:space:]]*=[[:space:]]*\[[[:space:]]*[0-9,[:space:]]*\]/s/^[#[:space:]]*/    /; s/default.clock.allowed-rates[[:space:]]*=[[:space:]]*\[[[:space:]]*[0-9,[:space:]]*\]/default.clock.allowed-rates = \[ %d \]/`, clockRate),
+		dir+"/pipewire.conf")
+
+	out1, errCmd := cmd1.CombinedOutput()
+	if errCmd != nil {
+		fmt.Println("Error:", err, "If /etc/pipewire it likely will be .conf.d files, try /.config/pipewire")
+	}
+	fmt.Println(string(out1))
 }
